32465a86f3774bce9e8a1bce9604eebdd89618e4
mikengugy <mikengugy@gmail.com>
2020-11-02 11:17:58 +0300

    added config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/mypersonalblog'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SECRET_KEY = 'Michael123'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'development':ProdConfig
+}
